db03cf088877f57163916b57acb355f0d7db4dd5
Dmytro Sapozhnyk <dimonsuper010@gmail.com>
2024-08-01 04:19:21 +0000

    Init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nested-loops-redux-saga
+
+demo for https://github.com/redux-saga/redux-saga/issues/1592
---
+++ index.js
@@ -0,0 +1,7 @@
+import { processBigLoop } from './loop.js'
+
+const main = async () => {
+    await processBigLoop();
+}
+
+main();
